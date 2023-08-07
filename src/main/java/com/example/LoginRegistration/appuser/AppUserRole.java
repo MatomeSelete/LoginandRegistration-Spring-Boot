d38e27c2e94980992cba8568f9c1bbdb4e220fd4
Matome <seletematome3@gmail.com>
2023-08-07 11:58:55 +0200

    done with user side

---
+++ src/main/java/com/example/LoginRegistration/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.example.LoginRegistration.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/example/LoginRegistration/appuser/AppUserService.java
@@ -0,0 +1,25 @@
+package com.example.LoginRegistration.appuser;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AppUserService implements UserDetailsService {
+
+    private final static String User_Not_Found_MSG =
+            "user with %s not found";
+    private final AppUserRepository appUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        return appUserRepository.findByEmail(email)
+                .orElseThrow(()->
+                        new UsernameNotFoundException(
+                                String.format(User_Not_Found_MSG, email)));
+    }
+}
